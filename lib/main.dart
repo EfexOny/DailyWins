f7e55388b7b96e15f6c8afc56ff78a1b4d4ef444
EfexOny <87447323+EfexOny@users.noreply.github.com>
2024-06-07 16:34:36 +0300

    nav+ui done(partly)

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData( 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: MainPage(),
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,8 +2,10 @@ import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:getwidget/components/button/gf_button.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:tracker/data/ProgressItem.dart';
 import 'package:tracker/screens/others/habit_card.dart';
@@ -15,6 +17,10 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 
 }
+
+List<Widget> screens = (
+  
+);
 
 class _MainPageState extends State<MainPage> {
   var _selectedTab = _SelectedTab.home;
@@ -47,26 +53,16 @@ CalendarFormat _calendarFormat = CalendarFormat.week;
 ];
 
     return Scaffold(
-      bottomNavigationBar: DotNavigationBar(
-        marginR: const EdgeInsets.only(bottom: 0, right: 40, left: 40),
-        paddingR: const EdgeInsets.only(bottom: 1, top: 5),
-        itemPadding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-        currentIndex: _SelectedTab.values.indexOf(_selectedTab),
-        duration: const Duration(milliseconds: 700),
-        backgroundColor: Color(0xFF242424),
-        onTap: _handleIndexChanged,
-        items: [
-            DotNavigationBarItem(
-              icon: Icon(Icons.home),
-              unselectedColor: Color(0xFF58a45c),
-              selectedColor: Colors.white,
-            ),
-            DotNavigationBarItem(
-              icon: Icon(Icons.hexagon),
-              unselectedColor: Color(0xFF58a45c),
-              selectedColor: Colors.white,
-            ),
-          ],),
+      bottomNavigationBar: GNav(backgroundColor: Color(0xFF222322),
+        onTabChange: (i){
+          print(i);
+        },
+        gap: 20,
+        tabs: [
+        GButton(icon: Icons.home_filled,text: "Home",iconActiveColor: Color(0xFF8f8e88),iconColor: Color(0xFF8f8e88),textColor: Colors.white),
+        GButton(icon: Icons.bar_chart_rounded,text:"Statistics",iconColor: Color(0xFF8f8e88),iconActiveColor: Color(0xFF8f8e88),textColor: Colors.white),
+        GButton(icon: Icons.hexagon_outlined,text:"????",iconColor: Color(0xFF8f8e88),iconActiveColor: Color(0xFF8f8e88),textColor: Colors.white,),
+      ]),
       backgroundColor: Color(0xFFf9fbed),
       body: Padding(
         padding: const EdgeInsets.only(top: 40.0,right: 20.0,left: 20.0),
@@ -85,9 +81,15 @@ CalendarFormat _calendarFormat = CalendarFormat.week;
                       focusedDay: DateTime.now(),firstDay: DateTime.utc(2024,5,20),lastDay: DateTime.utc(2024,6,20),
                       ),
                     Text("Go For Daily",style: GoogleFonts.montserrat(color:Colors.black,fontSize:50),),
-                    Text("Wins",style: GoogleFonts.montserrat(color:Colors.green,fontSize:110),),
+                    Text("Wins",style: GoogleFonts.montserrat(color:Color(0xFF4ca45c),fontSize:110),),
                     Divider(),
-                    Text("Habits",style: GoogleFonts.montserrat(color:Colors.black,fontSize:40,letterSpacing:4),),
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Habits",style: GoogleFonts.montserrat(color:Colors.black,fontSize:40,letterSpacing:4),),
+                        GFButton(shape: GFButtonShape.pills,color: Color(0xFF4ca45c),
+                          child: Text("+ New Habit",style: TextStyle(fontSize: 15,color: Colors.black),),onPressed: () => print("Add habit"),),
+                      ],
+                    ),
                     for(int i=0;i<progressItems.length;i++)
                       HabitCard(item: progressItems[i]),
               ],
